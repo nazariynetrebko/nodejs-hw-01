ee0e574f09d91e21698ef711440c95a7e37d832d
nazar_netrebko <netrebkon@gmail.com>
2025-06-27 18:36:07 +0300

    ready

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,11 @@
-const generateContacts = async (number) => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+
+const generateContacts = async (number) => {
+  const existingContacts = await readContacts();
+  const newContacts = Array.from({ length: number }, () => createFakeContact());
+  await writeContacts([...existingContacts, ...newContacts]);
+};
 
 generateContacts(5);
